00cdece2e07a25a8ebaa660007c64233a2fc7de6
vyshakkolloth <vyshakkolloth@gmail.com>
2025-08-04 15:34:05 +0530

    Fix cors error

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://recursive-react-application.vercel.app/',
   credentials: true
 }));
 // Routes
